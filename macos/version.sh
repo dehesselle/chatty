205751ed6d41d03102458e996d7fc95845debc99
René de Hesselle <dehesselle@web.de>
2017-04-13 16:13:39 +0200

    Change version, bump build number

--- macos/version.sh
+++ macos/version.sh
@@ -1,7 +1,7 @@
 # https://github.com/dehesselle/chatty
 
-CHATTY_VERSION=0.8.4.1
-CHATTY_MACOS_BUILD=8
+CHATTY_VERSION=0.8.5
+CHATTY_MACOS_BUILD=9
 
 echo "------------------------------"
 echo "Chatty $CHATTY_VERSION (build $CHATTY_MACOS_BUILD)"

ca3fd6325f41f755917012f0aacb126fe39285e9
René de Hesselle <dehesselle@web.de>
2017-04-29 16:12:41 +0200

    Use ‚points-at‘ instead of ‚describe‘
    This works now with multiple tags on the same commit.

--- macos/version.sh
+++ macos/version.sh
@@ -1,10 +1,14 @@
 # https://github.com/dehesselle/chatty
 
-GIT_DESCRIBE=$(git describe --always --tags)
 
-if [[ "$GIT_DESCRIBE" =~ v(.*)_b([0-9]+).* ]]; then
+GIT_TAG=$(git tag --points-at HEAD | tail -n 1)
+
+if [[ "$GIT_TAG" =~ v(.*)_b([0-9]+).* ]]; then
    CHATTY_VERSION=${BASH_REMATCH[1]}
    CHATTY_MACOS_BUILD=${BASH_REMATCH[2]}
+else
+   echo "***ERROR*** in version.sh"
+   exit 1
 fi
 
 echo "------------------------------"

3a2a0f9e7e2a7508873875bba99f4bcefbb6a8b4
René de Hesselle <dehesselle@web.de>
2017-05-28 14:18:23 +0200

    Allow to continue in case of error

--- macos/version.sh
+++ macos/version.sh
@@ -8,7 +8,15 @@ if [[ "$GIT_TAG" =~ v(.*)_b([0-9]+).* ]]; then
    CHATTY_MACOS_BUILD=${BASH_REMATCH[2]}
 else
    echo "***ERROR*** in version.sh"
-   exit 1
+
+   while true; do
+      read -p "Do you want me to continue?" RESULT
+      case $RESULT in
+         [Yy]* ) CHATTY_VERSION=0; CHATTY_MACOS_BUILD=0; break ;;
+         [Nn]* ) exit 1 ;;
+         * ) echo "Please answer yes or no.";;
+      esac
+   done
 fi
 
 echo "------------------------------"

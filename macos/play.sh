a26174ccbef833bb4bb94ff5ed4a8d869feb6ac3
René de Hesselle <dehesselle@web.de>
2021-01-19 21:26:35 +0100

    Add a way to disable a player

--- macos/play.sh
+++ macos/play.sh
@@ -11,6 +11,7 @@
 RESOURCES_DIR=$(dirname $0)/..
 PYTHON_BIN_DIR=$RESOURCES_DIR/../Frameworks/Python.framework/Versions/Current/bin
 SCRIPTS_DIR=$RESOURCES_DIR/scripts
+CONFIG_DIR=$HOME/Library/Application\ Support/Chatty
 ARGS="$*"                   # e.g. 'twitch.tv/channelname best'
 STREAM=${ARGS#twitch.tv/}   # remove all chars before 'channelname'
 STREAM=${STREAM% *}         # remove all args after 'channelname'
@@ -18,12 +19,16 @@ QUALITY=${ARGS#*$STREAM}    # retain quality argument after 'channelname'
 
 #--- main ----------------------------------------------------------------------
 
-source $SCRIPTS_DIR/iina.sh
-source $SCRIPTS_DIR/streamlink_vlc.sh
+if [ ! -f "$CONFIG_DIR/no_iina" ]; then
+   source $SCRIPTS_DIR/iina.sh
+fi
+
+if [ ! -f "$CONFIG_DIR/no_vlc" ]; then
+   source $SCRIPTS_DIR/streamlink_vlc.sh
+fi
 
 # On newer macOS versions, this will trigger a security dialag, asking for
 # permission to access system events.
-
 osascript <<EOT
    tell app "System Events"
       display dialog "Could not find a supported app for video playback. Either install IINA or VLC." buttons {"OK"} default button 1 with icon caution with title "no supported video player"
